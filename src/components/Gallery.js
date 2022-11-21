e3f749acec76f9d5f64970aba9fd5a40146c8bf8
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 15:21:34 +0000

    generate images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 const Gallery = () => {
-  
+  const [imageKeys, setImageKeys] = useState([]);
+
+  const url = 'https://fto-211122.s3.eu-west-1.amazonaws.com/';
+
+  const getImages = async () => {
+    const res = await axios.get('http://localhost:3000/s3/get_images');
+    setImageKeys(res.data.keys[0]);
+  };
+
+  const renderImages = () => {
+    return imageKeys.map((item, index) => {
+      return (
+        <div>
+          <img src={`${url}${item}`} alt="" key={index}/>
+        </div>
+      );
+    });
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <div>
-      <p>Images here</p>
+      <ul>{renderImages()}</ul>
     </div>
   );
 };
