2d53ba677053077aa8d1ccb054c95b72c1c76aa6
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 17:01:37 +0000

    add post methods for images to backend api

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,21 +1,33 @@
 import { Button, Form } from 'react-bootstrap';
 import { useState } from 'react';
+import axios from 'axios';
 
 const UploadForm = () => {
   const [image, setImage] = useState(null);
 
   const handleChange = (e) => {
     e.preventDefault();
-    console.log(e.target.files[0]);
     setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendImage();
+  };
+
+  const sendImage = async () => {
+    const res = await axios.post('http://localhost:3000/s3/direct_post', {
+      image: image,
+    });
+    console.log(res);
   };
 
   return (
     <div>
       <h1>Upload Image below</h1>
-      <Form onChange={(e) => handleChange(e)}>
+      <Form onChange={(e) => handleChange(e)} onSubmit={(e) => handleSubmit(e)}>
         {image && (
-          <div>
+          <div className="img-preview-wrapper">
             <h5>image preview</h5>
             <img src={image} alt="" className="img-preview" />
           </div>
