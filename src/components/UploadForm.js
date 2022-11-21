872a763f925bad878d94edee214c94c697ac1d58
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 16:26:43 +0000

    add image upload preview

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,16 +1,28 @@
-import { Button, Form } from "react-bootstrap";
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 const UploadForm = () => {
-  return <div>
+  const [image, setImage] = useState(null);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  return (
     <div>
       <h1>Upload Image below</h1>
-      <Form>
+      <Form onChange={(e) => handleChange(e)}>
+        <img src={image} alt="" className="img-preview" />
         <Form.Group>
-          <Form.Control type='file' name='myImage'></Form.Control>
+          <Form.Control type="file" name="myImage"></Form.Control>
+          <Button type="submit">Submit</Button>
         </Form.Group>
       </Form>
+      <hr />
     </div>
-  </div>;
+  );
 };
 
 export default UploadForm;
